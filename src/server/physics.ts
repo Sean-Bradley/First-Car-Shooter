c575cc34552eb427ab1a893864f754fcb3c719aa
Sean Bradley <seanwasere@gmail.com>
2022-01-13 08:56:31 +0000

    updates

--- src/server/physics.ts
+++ src/server/physics.ts
@@ -21,7 +21,7 @@ export default class Physics {
 
         const loader = new OBJLoader()
         const data = fs.readFileSync(
-            path.resolve(__dirname, '../client/models/topoEarth.obj'),
+            path.resolve(__dirname, '../client/models/topoEarth-2.obj'),
             { encoding: 'utf8', flag: 'r' }
         )
 
